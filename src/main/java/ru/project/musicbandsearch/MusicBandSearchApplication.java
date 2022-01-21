2e301a97757b6658b4bdd373ab83db4fb30a36b4
Nessing <35237174+Nessing@users.noreply.github.com>
2022-01-21 22:00:29 +0300

    Create master

---
+++ src/main/java/ru/project/musicbandsearch/MusicBandSearchApplication.java
@@ -0,0 +1,13 @@
+package ru.project.musicbandsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicBandSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicBandSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/project/musicbandsearch/MusicBandSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.musicbandsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicBandSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
