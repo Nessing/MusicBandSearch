f1649266fda7a41a050ee11ae564306647468f9f
Nessing <35237174+Nessing@users.noreply.github.com>
2022-01-22 00:44:44 +0300

    Создание сущностей только с полями "id"

---
+++ src/main/java/ru/project/musicbandsearch/entities/MusicBand.java
@@ -0,0 +1,17 @@
+package ru.project.musicbandsearch.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+//@Table(name = "")
+public class MusicBand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+}
